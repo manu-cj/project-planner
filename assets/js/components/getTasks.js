0fe1e4e5f173a956f1f4aa244cdcad135f90ce5c
Caroline <deconinck.caro@gmail.com>
2024-05-06 13:52:21 +0200

    added tasks.scss, modified task class, created display task

--- assets/js/components/getTasks.js
+++ assets/js/components/getTasks.js
@@ -2,7 +2,7 @@ import { Task } from "./Task.js"
 
 const getTasksFromStorage = () => {
     const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    return tasks.map(taskObj => new Task(taskObj.name, taskObj.deadline, taskObj.description, taskObj.id));
+    return tasks.map(taskObj => new Task(taskObj.name, taskObj.deadline, taskObj.description, taskObj.id, taskObj.status));
 };
 
 export { getTasksFromStorage };
--- assets/js/components/showTasks.js
+++ assets/js/components/showTasks.js
@@ -1,24 +1,31 @@
 import { getTasksFromStorage } from "./getTasks.js";
 
-
 const displayTasks = () => {
     const tasks = getTasksFromStorage();
-    const taskContainer = document.querySelector(".test-container");
+    const toDoTree = document.querySelector("#toDo");
+    const doingTree = document.querySelector("#doing");
+    const doneTree = document.querySelector("#done");
     for (let task of tasks) {
+        console.log(task.status)
         const taskCard = document.createElement("div");
         taskCard.classList.add("task");
         taskCard.innerHTML = 
         `
-        <h2>${task.name}</h2>
-        <p>${task.description}</p>
-        
-        
+            <p class="controls"><img title="Edit Task" class="edit_task" id="edit_${task.id}" src="assets/images/edit.svg"><img title="Delete Task" class="delete_task" id="delete_${task.id}" src="assets/images/delete.svg"</p>
+            <h3>${task.name}</h3>
+            <p>${task.description}</p>
+            <p><img src="assets/images/clock.svg">${task.deadline}</p>
         `
-
-
-        taskContainer.appendChild(taskCard);
+        if (task.status === "done") {
+            doneTree.insertBefore(taskCard, doneTree.querySelector(".add"));
+        }
+        else if (task.status === "doing") {
+            doingTree.insertBefore(taskCard, doingTree.querySelector(".add"));
+        }
+        else {
+            toDoTree.insertBefore(taskCard, toDoTree.querySelector(".add"));
+        }
     }
-
 }
 
 export {displayTasks}
