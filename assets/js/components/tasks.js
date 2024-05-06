5ca01442802afff01bf28cfe6a91da3cbb93b245
Caroline <deconinck.caro@gmail.com>
2024-05-06 10:58:09 +0200

    modified getTasksFromStorage function to retrieve actual instances

--- assets/js/components/tasks.js
+++ assets/js/components/tasks.js
@@ -1,6 +1,8 @@
+import { Task } from "./Task.js"
+
 const getTasksFromStorage = () => {
     const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    return [...tasks];
+    return tasks.map(taskObj => new Task(taskObj.name, taskObj.deadline, taskObj.description, taskObj.id));
 };
 
 export { getTasksFromStorage };
