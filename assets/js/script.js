fa3bfad6c93f92faef2a5cca661009e49bc5b7eb
manu-cj <carenjotje@gmail.com>
2024-05-06 11:17:13 +0200

    add modal componnent

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+import modal from './component/modal.js';
+
+/*Donner lui le type de tache, le texte du bouton submit et 
+l'index, à appeler lors de l'advEventListenner de l'ajout de tache et update*/
+// let button = document.querySelector('button');
+//     button.addEventListener('click', () => {
+//         modal('todo', 'add-task', 1);
+//     })
