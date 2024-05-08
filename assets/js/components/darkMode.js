d27830a965dcfc538417b6fdee36fdd7511ad954
Caroline <deconinck.caro@gmail.com>
2024-05-08 11:41:54 +0200

    added darkMode styling to filter menu

--- assets/js/components/darkMode.js
+++ assets/js/components/darkMode.js
@@ -1,19 +1,22 @@
 let body = document.querySelector('body');
 let header = document.querySelector('header');
 let main = document.querySelector('main');
+let filterMenu = document.querySelector('.filter-menu');
 
 
 export function darkMode () {
     body.classList.add('darkMode');
     header.classList.add('darkMode');
     main.classList.add('darkMode');
+    filterMenu.classList.add('darkMode');
     localStorage.setItem('darkModeEnabled', 'true');
 }
 
 export function clear () {
     body.classList.remove('darkMode');
     header.classList.remove('darkMode');
-    main.classList.remove('darkMode');  
+    main.classList.remove('darkMode');
+    filterMenu.classList.remove('darkMode');
     localStorage.setItem('darkModeEnabled', 'false');
 }
 
