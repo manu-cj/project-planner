e9b578e745fb7b7f3212c028e53af7ba40d8edc3
manu-cj <carenjotje@gmail.com>
2024-05-08 18:43:05 +0200

    add style for footer

--- assets/js/components/darkMode.js
+++ assets/js/components/darkMode.js
@@ -1,6 +1,7 @@
 let body = document.querySelector('body');
 let header = document.querySelector('header');
 let main = document.querySelector('main');
+let footer = document.querySelector('footer');
 let filterMenu = document.querySelector('.filter-menu');
 
 
@@ -9,6 +10,7 @@ export function darkMode () {
     header.classList.add('darkMode');
     main.classList.add('darkMode');
     filterMenu.classList.add('darkMode');
+    footer.classList.add('darkMode');
     localStorage.setItem('darkModeEnabled', 'true');
 }
 
@@ -17,6 +19,7 @@ export function clear () {
     header.classList.remove('darkMode');
     main.classList.remove('darkMode');
     filterMenu.classList.remove('darkMode');
+    footer.classList.remove('darkMode');
     localStorage.setItem('darkModeEnabled', 'false');
 }
 
